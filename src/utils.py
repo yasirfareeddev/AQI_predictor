4226a5e1fe178f035ac644cc9b543949386f54b6
yasirfareeddev <m.yasirfareed2002@gmail.com>
2026-06-03 03:30:07 +0500

    Add retry logic

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,33 @@
 import os
-from pymongo import MongoClient
+import time
+from pymongo import MongoClient, errors
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_mongo_client():
     uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-    client = MongoClient(uri)
-    return client["aqi_db"]
+
+    for attempt in range(3):
+        try:
+            client = MongoClient(
+                uri,
+                serverSelectionTimeoutMS=30000,
+                connectTimeoutMS=30000,
+                socketTimeoutMS=30000,
+                retryWrites=True,
+                retryReads=True
+            )
+
+            client.admin.command("ping")
+            print("MongoDB connected")
+
+            return client["aqi_db"]
+
+        except errors.ServerSelectionTimeoutError as e:
+            print(f"MongoDB connection failed (attempt {attempt+1}/3)")
+
+            if attempt < 2:
+                time.sleep(5)
+            else:
+                raise e
